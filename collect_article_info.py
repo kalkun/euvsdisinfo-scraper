c15c7061890888bd70684a2fc0fb4ccc03041102
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2017-10-10 17:27:05 +0200

    started report, finished first version of a collect_article_info to extract content information through meta tags from sources

--- collect_article_info.py
+++ collect_article_info.py
@@ -3,12 +3,20 @@
 """
     This script will help collecting meta data over a collection of URLs.
     The meta data will be based on header tags such as those used for SEO optimizations, title tag or similar.
+
+    The following is an example of describing a wikipedia article on yellow journalism:
+    ```
+    from collect_article_info import describe_article as da
+    da("https://en.wikipedia.org/wiki/Yellow_journalism")
+    ```
+
 """
 
 import requests as req
 from bs4 import BeautifulSoup as bs
 import json
 import csv
+import re
 """
 * metatag_list is a list of triples where the first are the key value pairs to
 * use as filters for relevant tags, the last is the name of the attribute for which
@@ -32,53 +40,85 @@ metatag_list = [
 ]
 tag_list = [ ("h%s" % i, None) for i in range(1, 7) ] + [("title", None)]
 
-def describe_page(url):
-    page = req.get(url)
-    soup = bs(page.text, 'lxml')
-    info = {}
+whitespace = re.compile("(\r|\n|\t|u'\xa0')+")
+def rm_whitespace(s): return re.sub(whitespace, " ", s).strip()
+
+def describe_article(url):
+    page = req.get(url, headers={"User-Agent": "chrome"})
+    soup = bs(page.content, 'lxml')
+    metatags = {}
     for tag in metatag_list:
         try:
             content = ""
             for x in soup.find_all("meta", {tag[0]: tag[1]}):
                 content += x[tag[2]]
             if content != "":
-                info[tag[0] + '_' + tag[1]] = content.strip()
+                metatags[tag[0] + '_' + tag[1]] = content.strip()
         except:
             print("Failed to find meta tag %s with value %s" % (tag[0], tag[1]))
             continue
     for tag in tag_list:
         try:
             content = ""
-            for x in soup.find_all(tag[0]):
-                if not tag[1] is None:
-                    content += x[tag[1]]
-                else:
-                    content += " " + x.text
+            #for x in soup.find(tag[0]):
+            x = soup.find(tag[0])
+            if not tag[1] is None:
+                content += x[tag[1]]
+            else:
+                content += " " + x.text
             if content != "":
                 i = 1
-                while tag[0] + "_%s" % i in info:
+                while tag[0] + "_%s" % i in metatags:
                     i += 1
-                info[tag[0] + "_%s" % i] = content.strip()
+                metatags[tag[0] + "_%s" % i] = content.strip()
+            #print("Could find metatags for tag %s\n%s" % (tag[0], x.text))
         except:
-            print("Could not find info for tag %s" % tag[0])
+            #print("Could not find metatags for tag %s" % tag[0])
             continue
-    print("{")
-    for k, v in info.items():
-        print("\t%s :\t%s," % (k, v))
-    print("}")
-    return info
+
+    tags = ""
+    info = ""
+    for k, v in metatags.items():
+        # collect info gathered in metatags, but try
+        # to exclude duplicate information
+        v = rm_whitespace(v)
+        if (info in v and info != "") or v in info:
+            continue
+        elif info == "":
+            info += v
+        elif info[-1] != " ":
+            info += " " + v
+        else:
+            info += v
+        tags += k if tags == "" else ", " + k
+    print("\t%s" % info)
+    return tags.strip(), info
 
 if __name__ == "__main__":
-    with open("./first_data.csv", "r") as f:
-        reader = csv.reader(f, delimiter=";", quotechar="\"")
-        header = {v: k for k, v in enumerate(next(reader))}
-        print(header)
-        for row in reader:
-            source = header['source']
-            url = row[source]
-            print("_" * 80)
-            print(url)
-            description = describe_page(url)
-#            print(row[header['source']])
-#            describe_page(row[header[
+    def dump_extracts(rows):
+        with open("test_extraction.csv", "w") as f:
+            writer = csv.writer(f, delimiter=",", quotechar="\"", quoting=csv.QUOTE_ALL)
+            writer.writerows(rows)
+    try:
+        with open("./full_dataset_formatted.csv", "r") as f:
+            reader = csv.reader(f, delimiter=",", quotechar="\"")
+            orig_header = next(reader)
+            header = {v: k for k, v in enumerate(orig_header)}
+            print(header)
+            newRows = [
+                orig_header + ["metatags", "metatags_content"] # new header row for new csv
+            ]
+            for row in reader:
+                source = header['source']
+                url = row[source]
+                print("_" * 80)
+                print(url)
+                print("\n%s" % row[header['language']])
+                newRows.append(row + list(describe_article(url)))
+    except KeyboardInterrupt:
+        print("Caught interrupt, dumping data")
+        dump_extracts(newRows)
+        print("Data dumped")
+    # if finished, dump data:
+    dump_extracts(newRows)
 
